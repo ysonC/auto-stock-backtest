10ae3818b2d0d80d84945617b63f074ae9455df5
Wyson Cheng <efyhc5@nottingham.ac.uk>
2025-01-01 15:39:13 -0500

    test app

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    return "Hello, World!"
 
 @app.route('/fetch', methods=['POST'])
 def fetch_stocks():
